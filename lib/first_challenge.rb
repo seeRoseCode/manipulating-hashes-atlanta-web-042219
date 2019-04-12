064c20c9d476fd4b2157120df2d2d2d676611819
Kesha C Lake <keshalakemusic@gmail.com>
2019-04-12 03:29:32 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 def first_challenge
   contacts = {
     "Jon Snow" => {
@@ -11,17 +13,19 @@ def first_challenge
       email: "freddy@mercury.com",
       favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
     }
+
   }
 
   #your code here
   contacts.collect do |person, data|
     data.collect do |value, attritute|
-      if attritute == :favorite_icecream_flavors
-        if :favorite_icecream_flavors.include? "strawberry"
-          :favorite_icecream_flavors.delete("strawberry")
+      if value == :favorite_icecream_flavors
+        if :favorite_icecream_flavors.include("strawberry")
+        :favorite_icecream_flavors.delete("strawberry")
         end
       end
     end
+  end
   #remember to return your newly altered contacts hash!
   contacts
 end
